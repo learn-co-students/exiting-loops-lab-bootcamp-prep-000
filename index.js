e5e995d0e49c769b757002c64682e2a59ad26215
Alejandro Coca <amcocarojas@gmail.com>
2016-09-10 02:21:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function breakOut(array, changeValue, stopValue) {
+
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] === stopValue){
+        break
+    }
+    array[i] = changeValue;
+  }
+
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] === skipValue){
+        continue
+    }
+    array[i] = changeValue;
+  }
+
+  return array;
+}
+
+function findBy(array, findFn) {
+
+  var result = null;
+
+  for (var i = 0; i < array.length; i++) {
+    if(findFn(array[i])){
+        result = array[i];
+    }
+  }
+
+  return result;
+}
