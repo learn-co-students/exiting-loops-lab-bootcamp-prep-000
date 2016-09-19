58f88cccda4c4e3d2c71d1073ee34bddf88c3aa4
Justin Mayerick <jmayeric@gmail.com>
2016-09-19 17:35:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function breakOut(array, changeValue, stopValue) {
-  for (var i = 0, l = array.length; i < l; i++) {
-    if (array[i] === stopValue) {
-      break
-    } else {
-      array[i] = changeValue
-    }
-  }
-  return array
-}
-
-function keepGoing(array, changeValue, skipValue) {
-  for (var i = 0, l = array.length; i < l; i++) {
-    if (array[i] === skipValue) {
-      continue
-    } else {
-      array[i] = changeValue
-    }
-  }
-  return array
-}
-debugger;
-function findBy(array, findFn) {
-  var found = null
-  for (var i = 0, l = array.length; i < l; i++) {
-    if (array[i].hasOwnProperty(findFn)) {
-      found = array[i]
-    }
-  }
-  return found
-}
+// function breakOut(array, changeValue, stopValue) {
+//   for (var i = 0, l = array.length; i < l; i++) {
+//     if (array[i] === stopValue) {
+//       break
+//     } else {
+//       array[i] = changeValue
+//     }
+//   }
+//   return array
+// }
+//
+// function keepGoing(array, changeValue, skipValue) {
+//   for (var i = 0, l = array.length; i < l; i++) {
+//     if (array[i] === skipValue) {
+//       continue
+//     } else {
+//       array[i] = changeValue
+//     }
+//   }
+//   return array
+// }
+// debugger;
+// function findBy(array, findFn) {
+//   var found = null
+//   for (var i = 0, l = array.length; i < l; i++) {
+//     if (array[i].hasOwnProperty(findFn)) {
+//       found = array[i]
+//     }
+//   }
+//   return found
+// }
