f8c1e553199c71d0292ccaf2cde85c61099eb360
JORGE HERNANDEZ <jnandez20@yahoo.com>
2016-09-20 19:23:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var breakOut = (array, changeValue, stopValue) => {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue;
+    }else {
+      break;
+    }
+  }
+  return array;
+}
+
+var keepGoing = (array, changeValue, skipValue) => {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+var findBy = (array, findFn) => {
+  for (var i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+  return null;
+}
