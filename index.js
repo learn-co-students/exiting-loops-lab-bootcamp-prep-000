b98482b90e1a385c90836f4a7f9b79bc29d3c200
Matt Borg <msimonborg@gmail.com>
2016-10-06 17:27:28 +0000

    Done.

--- index.js
+++ index.js
@@ -24,29 +24,7 @@ function findBy ( array, findFn ) {
 
 
 
-const person = {
-  name: "Awesome Name" ,
-  occupation: {
-      title: "Senior Manager of Awesome" ,
-      yearsHeld: 2
-  },
-  pets: [{
-      kind: 'dog' ,
-      name: 'Fiona'
-  } , {
-      kind: 'cat' ,
-      name: 'Ralph'
-  }]
-}
-
-function find ( array, criteriaFn ) {
-  for ( let i = 0, l = array.length; i < l; i++ ) {
-    if (criteriaFn ( array[i] )) {
-      return array[i] ;
-    }
-  }
-}
-
+// breadth-width search, arrays only
 function find ( array, criteriaFn ) {
   let current = array ;
   let next = [] ;
@@ -56,6 +34,31 @@ function find ( array, criteriaFn ) {
     }
     if ( Array.isArray (current) ) {
       for ( let i = 0, l = current.length; i < l; i++ ) {
+        next.push ( current[i] ) ;
+      }
+    }
+    current = next.shift() ;
+  }
+  return null ;
+}
+
+
+// breadth-width search, objects and arrays
+function find ( arrayOrObject, criteriaFn ) {
+  let current = arrayOrObject ;
+  let next = [] ;
+  while ( current ) {
+    if ( criteriaFn ( current )) {
+      return current ;
+    }
+    if ( Array.isArray (current) ) {
+      for ( let i = 0, l = current.length; i < l; i++ ) {
+        next.push ( current[i] ) ;
+      }
+    }
+    if ( !Array.isArray (current) && typeof current === 'object' ) {
+      for ( let i in current ) {
+        next.push ( i ) ;
         next.push ( current[i] ) ;
       }
     }
