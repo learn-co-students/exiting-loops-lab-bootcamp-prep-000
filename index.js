e640054dd5c03953ce07ce7505eb44c60921fcad
Bonnie Brewer <bj.brewer@att.net>
2016-10-07 00:09:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function breakOut(array,changeValue,stopValue) {
   for (var i=0; i<array.length;i++) {
-    if (array[i]==stopValue) {break}
+    if (array[i]===stopValue) {break}
     else {array[i]=changeValue;}
   }
   return array;
 }
 
-keepGoing(array,changeValue,skipValue)
+function keepGoing(array,changeValue,skipValue) {
+  for (var i=0; i<array.length;i++) {
+    if (array[i]===skipValue) {continue}
+    else {array[i]=changeValue;}
+  }
+  return array
+}
+
+function findBy(array,findFn) {
+  for (var i=0; i<array.length;i++) {
+    if (findFn(array[i])) {return array[i]}
+  }
+  return null
+}
