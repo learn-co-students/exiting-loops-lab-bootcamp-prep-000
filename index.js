cda9b242ce883f338e31503ee0f91d08e231327b
Airyque Ervin <airyque@gmail.com>
2016-10-19 03:01:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue
+      break
+    }
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[!] == skipValue) {
+      break
+    }else {
+      return array
+    }
+  }
+}
+
+function findBy(array, findFn) {
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  if (array == findFn) {
+
+  } else {
+    return null;
+  }
+}
