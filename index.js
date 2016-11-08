dea50986cec628dc5541fe18d81a876a3005db31
John Shelby <johnshelby@yahoo.com>
2016-11-08 00:23:04 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ function breakOut(array,changeValue,stopValue) {
       }
     }return array
 }
+
+function keepGoing(array,changeValue,skipValue) {
+  for(var i = 0; i < array.length; i++){
+    if (array[i] === skipValue){
+      continue}
+    else {
+      array[i] = changeValue
+      }
+    }
+  return array
+  }
+
+function findBy(array, findFn) {
+  for(var i = 0, l =array.length;i<l;i++){
+    if (findFn(array[i])===true) {
+      return array[i]
+    }
+  }
+  return null
+}
