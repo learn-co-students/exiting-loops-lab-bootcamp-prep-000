612c171b4cdbbf200cad5f991059a0fe7a0cacea
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-11-22 19:56:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function breakOut(array, changeValue, stopValue){
 for(let i = 0, l = array.length; i < l; i++){
 if (array[i] !== stopValue) {
-array = changeValue[i]}
+array.forEach(changeValue[i])}
 break
 }
   return array
 }
 
 function keepGoing(array, changeValue, skipValue){
-
-
+for(let i = 0, l = array.length; i < l; i++)
+if(array[i] !== skipValue){ continue
+  array.forEach(changeValue[i])
+}
   return array
+}
+
+
+function findBy(array, findFn) {
+for (let i =0, l = array.length; i <l; i++)
+if(findFn(array[i])){return array[i]}
+
 }
