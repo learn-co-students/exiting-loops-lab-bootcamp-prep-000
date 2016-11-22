1d3ca61dcc0f6c2e97b6e338e7c242748842c6fd
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-11-22 18:14:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function breakOut(array, changeValue, stopValue){
+for(i = 0; l = array.length i < l; i++){
+if ([i] !== stopValue)
+
+array.forEach([changeValue])
+}
+
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue){
+
+
+  return array
+}
