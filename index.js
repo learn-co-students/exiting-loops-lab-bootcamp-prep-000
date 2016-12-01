bb799b595568eff925b64f2b41637b3d2639a66b
Young Chul Lim <youngchullim@yahoo.com>
2016-12-01 13:43:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function breakOut(array, changeValue, stopValue) {
+  for(var i=0; i<array.length; i++) {
+    if(array[i] === stopValue) {
+      break;
+    }
+    else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for(var i=0; i<array.length; i++) {
+    if(array[i] !== skipValue) {
+      array[i] = changeValue;
+    }
+    else {
+      skipValue = skipValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array,findFn) {
+  for(var i=0; i<array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+}
+}
+}
