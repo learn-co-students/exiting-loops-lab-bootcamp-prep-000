3da931150c2e17ddb9f3bffb718c6020a58532bc
Tim Freeman <d.timothy.freeman@gmail.com>
2016-12-01 00:35:35 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0, l = array.length; i < l; i++) {
     if (findFn(array[i])) {
       return array[i];
       break;
