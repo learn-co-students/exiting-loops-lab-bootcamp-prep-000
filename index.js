9df445b2c483c7b339433e2aef00c0bf3429007b
jason brown <j.grant.brown@gmail.com>
2016-12-07 14:42:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function breakOut(array, changeValue, stopValue){
+  for(let i = 0, l  = array.length; i,l; i++){
+    array[i].push(changeValue)
+    if(array[i] == stopValue){
+      break
+    }
+  }}
