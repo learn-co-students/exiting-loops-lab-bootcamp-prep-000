48fe83cd3827c607d8f11fc77777d9f14e1a0cf3
David Ordovskiy <david.ordovsky@gmail.com>
2016-12-18 21:28:16 +0000

    Done.

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
-      return array[i];
-    } else {
-      return null;
+      return array[i]; 
     }
   }
 }
