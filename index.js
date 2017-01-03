e2dacc9be9d93db6607a500abf4a72ef93ceb49f
Aidan Bane <aidanfbane@gmail.com>
2017-01-03 23:12:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function breakOut(array, changeValue, stopValue) {
+  return arr;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  return array;
+}
+
+function findBy(array, findFn) {
+  return array;
+}
