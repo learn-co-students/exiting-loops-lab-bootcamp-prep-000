59bf58233f8c56af615872834ad32824300dc719
Paul Yu <phyu26@gmail.com>
2017-01-07 22:07:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function breakOut(array, changeValue, stopValue) {
+  for(let i=0, l=array.length; i<l; i++) {
+    if(array[i] !== stopValue) {
+      array[i] = changeValue;
+    } else break
+  } return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for(let i=0, l=array.length; i<l; i++) {
+    if(array[i] !== skipValue) {
+      array[i] = changeValue;
+    } else continue
+  } return array
+}
+
+function findBy(array, findFn) {
+  for(let i=0, l=array.length; i<l; i++) {
+    if(findFn(array[i])) {
+      return array[i];
+    } else continue
+  }
+}
