079731bf1508c78360de8e76fa9bb634fbd45ed1
Derrick Chu <dchu74@gmail.com>
2017-01-15 21:06:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function breakOut(array, changeValue, stopValue) {
+var array = [1,1,1,2,1,1]
+for (var i = 0; i < array.length; i++) {
+  array[i] = changeValue
+} break {
+  
+}
+
+  return array;
+}
