776bf01d8daf6e44c3c619bbbd650f092f48193f
Derrick Chu <dchu74@gmail.com>
2017-01-16 19:02:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 function breakOut(array, changeValue, stopValue) {
-var array = [1,1,1,2,1,1]
-for (var i = 0; i < array.length; i++) {
-  array[i] = changeValue
-} break {
-  
+  for (var i = 0, a = array.length; i < a; i++) {
+    if (array[i] === stopValue) {
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
 }
 
-  return array;
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0, a = array.length; i < a; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+//findBy(array, findFn) which looks for a value
+//in array based on the return value of findFn.
+//Return null if the value isn't found.
+
+function findBy(array, findFn) {
+  for (var i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+  return null
 }
