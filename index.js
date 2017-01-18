77f266636abcf38e5d3803f44c56bde705eb03cf
Mike Tangaro <mtangaro@gmail.com>
2017-01-18 18:41:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === stopValue) {
+      changeValue.array[i]);
+      break
+    }
+    i++;
+  }
+  return array
+}
