ab17b26a0d3a9eb05c00946828652c3024baa5ec
Art Mour <artmour@gmail.com>
2017-01-20 06:26:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i=0, l=array.length; i<l; i++) {
+    if (array[i] === stopValue) {
+      return array
+    } else {
+      array[i] = changeValue
+    }
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i=0, l=array.length; i<l; i++) {
+    if (array[i] === skipValue) {
+      continue
+    } else {
+      array[i] = changeValue
+    }
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+  for (let i=0, l=array.length; i<l; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    } else {
+      continue
+    }
+  }
+  return null
+}
