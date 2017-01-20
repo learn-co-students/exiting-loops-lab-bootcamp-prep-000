1bc60448072176ea12a4294c5434a87725adf509
charlie stopek <cstopek@hotmail.com>
2017-01-20 22:25:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0, n = array.length; i < n ; i++) {
+    if (array[i] == stopValue) {
+    break
+  }
+  else {array[i] = changeValue}
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+    for (let i = 0, n = array.length; i < n ; i++) {
+      if (array[i] == skipValue) {
+        continue
+      }
+      else {array [i] = changeValue}
+    }
+return array
+}
+
+function findBy(array, findFn) {
+  for (let i = 0, n = array.length; i < n ; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+      return null
+
+}
