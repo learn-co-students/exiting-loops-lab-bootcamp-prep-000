214fbd601266ce0e21716d4f162aeafcb4d25bba
Luis Rincon <rincon_92@hotmail.com>
2017-01-31 22:37:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function breakOut(array, changeValue, stopValue) {
-  array.forEach(element => {
-    array[] = changeValue
-  })
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === stopValue) {
+      break
+    }
+
+    array[i] = changeValue
+  }
+
+  return array
 }
+
 
 function keepGoing(array, changeValue, skipValue) {
-  array.forEach()
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+
+    array[i] = changeValue
+  }
+
+  return array
 }
 
+
 function findBy(array, findFn) {
-  array.forEach()
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+
+  return null
 }
