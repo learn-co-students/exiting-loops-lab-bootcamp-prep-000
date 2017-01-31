a52dc8e0d7e8e887f820c7960ab690f32e27f03b
Luis Rincon <rincon_92@hotmail.com>
2017-01-31 22:31:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function breakOut(array, changeValue, stopValue) {
+  array.forEach(element => {
+    array[] = changeValue
+  })
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  array.forEach()
+}
+
+function findBy(array, findFn) {
+  array.forEach()
+}
