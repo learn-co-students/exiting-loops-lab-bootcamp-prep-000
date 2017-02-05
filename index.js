0222a8ff44bd82ca448b8fcfbfe81eb05dbf4457
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-02-05 00:08:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function breakOut(array, changeValue, stopValue){
-  array.forEach(element => {
+  for(let i = 0, l = array.length; i<l; i++){
     if(element === stopValue){
       break;
     }
     element = changeValue;
-  })
+  }
   return array;
 }
 
 function keepGoing(array, changeValue, skipValue){
   array.forEach(element => {
     if(element === skipValue){
-      continue;
+      return;
     }
     element = changeValue;
   }
-  return array;
+  return array
 }
 
 function findBy(array, findFn){
