a552487509c56aa14ac8512cdc4f1d8ee04d677e
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2017-02-11 20:01:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function breakOut(array, changeValue, stopValue){
+  for (var i = 0; array.length = 1; i < 1, i++)
+    if (array[i] != changeValue){
+      array[i] = changeValue
+    } else {
+      break
+    }
+
+}
+
+
+function keepGoing(array, changeValue, skipValue){
+  for (let i = 0; 1 = array.length; i < 1; i++)
+    if(array[i] = changeValue){
+      continue
+    }
+}
+
+function findBy(array, findFn) {
+
+}
