bbf5e95c51a4897dfcc72a80b3d00ad1ce14eb53
subash karki <skarki35205@hotmail.com>
2017-02-24 02:30:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function breakOut(array, changeValue, stopValue){
+    for (var i = 0; i < array.length; i++) {
+        if (changeValue === stopValue) {
+            break
+        }
+        return array;
+    }
+}
