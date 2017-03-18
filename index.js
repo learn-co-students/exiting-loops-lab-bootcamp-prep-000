1a889dee36dad0ecb61e01adad5c2b32c72eb5c8
Mollie Stein <mollie.stein@gmail.com>
2017-03-18 19:21:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function breakOut(array, changeValue, stopValue){
+  for(var i=0; i < array.length; i++){
+    if (array[i] !== stopValue) {
+      array[i] = changeValue;
+    } else {
+      break
+    }
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue){
+  for(var i=0; i < array.length; i++){
+    if (array[i] === skipValue) {
+      continue
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array
+}
+
+/*function findBy(array, findFn){
+  array.forEach(function callback(element,index,array){
+//    if(findFn(element)){
+      if(findFn(array[index])){
+      return array[index]
+  }   else{
+      return null
+  }
+  })
+}*/
+
+function findBy(array, findFn) {
+  for (let i=0; i < array.length; i++) { 
+      if (findFn(array[i])) {
+      return array[i];
+      break;
+    }  
+  }
+}
