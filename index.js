470af507386128fa992030f720242b579a93fe90
Shalin Scupham <shalin.scupham@gmail.com>
2017-03-24 00:22:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-function breakout(array,changeValue,stopValue) {
+function breakOut(array,changeValue,stopValue) {
   for (var i = 0; i < array.length; i++) {
-    if (array = stopValue) {break};
+    if (array[i] === stopValue) {break}
     else {
       array[i] = changeValue
-      return array
     }
   }
+  return array
 }
 
 
 function keepGoing(array,changeValue,skipValue) {
-
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {continue}
+    else {
+      array[i]=changeValue
+    }
+  }
+  return array
 }
 
 function findBy(array,findFn){
-
-}
+  for (var i = 0; i < array.length; i++) {
+    if (findFn(array[i]))
+      return array [i]
+    }
+  return null
+  }
