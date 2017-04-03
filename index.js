530902704aeb7d13d2fa95cdb7fe174832621f22
Chris Wood <woodcr5@gmail.com>
2017-04-03 02:59:55 +0000

    Done.

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ function breakOut(array, changeValue, stopValue) {
 
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0, l = array.length; i < l; i++) {
-    if (array[i] !== skipValue) {
-      array[i] = changeValue
+    if (array[i] === skipValue) {
+      continue
     }
+    array[i] = changeValue
   }
   return array
 }
