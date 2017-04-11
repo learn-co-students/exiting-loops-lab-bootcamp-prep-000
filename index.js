f50e4eae01a9967def27522ea0049805be716532
Steven Ossorio <codebusiness2014@gmail.com>
2017-04-11 18:35:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function breakOut(array, changeValue, stopValue){
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == stopValue) {
+      break;
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue){
+
+}
+
+function findBy(array, findFn){
+
+}
