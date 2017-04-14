7a21e1f93b8285b24933db546a8a53505228be7a
Rena Sha <linkena@gmail.com>
2017-04-14 02:28:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function breakOut(array, changeValue, stopValue){
+
+
+  for(var i=0; i<array.length;i++){
+    if(array[i]===stopValue){
+      break;
+    }else{
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue){
+  for(var i = 0; i<array.length; i++){
+    if(array[i]!== skipValue){
+      array[i] = changeValue;
+    }else{
+      continue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn){
+  for(var i = 0; array.length; i++){
+    if(1){
+      return findFn;
+    }
+  }
+  return null;
+}
